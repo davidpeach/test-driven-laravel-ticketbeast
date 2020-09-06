646f3a4cd97e4c123f990ff00565c173d7430282
David Peach <mail@davidpeach.co.uk>
2020-09-06 12:16:08 +0100

    Dealing with lingering state

--- tests/Unit/Billing/StripePaymentGatewayTest.php
+++ tests/Unit/Billing/StripePaymentGatewayTest.php
@@ -10,12 +10,28 @@ use Tests\TestCase;
 
 class StripePaymentGatewayTest extends TestCase
 {
-    /** @test */
-    public function charges_with_a_valid_payment_token_are_successful()
+    private function lastCharge()
     {
-        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
+        return \Stripe\Charge::all(
+            ['limit' => 1],
+            ['api_key' => config('services.stripe.secret')]
+        )['data'][0];
+    }
 
-        $token = \Stripe\Token::create([
+    private function newCharges()
+    {
+        return \Stripe\Charge::all(
+            [
+                'limit' => 1,
+                'ending_before' => $this->lastCharge->id
+            ],
+            ['api_key' => config('services.stripe.secret')]
+        )['data'];
+    }
+
+    private function validToken()
+    {
+        return \Stripe\Token::create([
             'card' => [
                 'number' => '4242424242424242',
                 'exp_month' => 1,
@@ -23,14 +39,22 @@ class StripePaymentGatewayTest extends TestCase
                 'cvc' => '123',
             ],
         ], ['api_key' => config('services.stripe.secret')])->id;
+    }
 
-        $paymentGateway->charge(2500, $token);
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->lastCharge = $this->lastCharge();
+    }
 
-        $lastCharge = \Stripe\Charge::all(
-            ['limit' => 1],
-            ['api_key' => config('services.stripe.secret')]
-        )['data'][0];
+    /** @test */
+    public function charges_with_a_valid_payment_token_are_successful()
+    {
+        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
 
-        $this->assertEquals(2500, $lastCharge->amount);
+        $paymentGateway->charge(2500, $this->validToken());
+
+        $this->assertCount(1, $this->newCharges());
+        $this->assertEquals(2500, $this->lastCharge()->amount);
     }
 }
